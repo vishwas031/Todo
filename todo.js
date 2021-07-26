0c685dd4bfbd0d81cb51f9e7d39164dca4d9bd6f
Vishwas Singh <031vishwas@gmail.com>
2021-07-26 21:08:37 +0530

    made some changes in the color management system and Add todo function completed

--- todo.js
+++ todo.js
@@ -1,24 +1,58 @@
-const input = document.getElementById("#input")
-const add = document.getElementById("#add")
-const remove = document.getElementById("#remove")
-const removeall = document.getElementById("#removeall")
-const ul = document.getElementsByClassName(".list")
+var purple = document.querySelector('.purple');
+var red = document.querySelector('.red');
+var green = document.querySelector('.green');
+var blue = document.querySelector('.blue');
+var black = document.querySelector('.black');
 
-addEle = ()=> {
-  const list = document.createElement('li');
-  list.innerText = `${input}`;
-  ul[0].appendChild(list);
+var secondary = '#af7eeb';
 
+
+var ul = document.querySelector('#LIST');
+var addNote = document.querySelector('.fa-plus');
+var input = document.getElementById('input');
+
+var li = document.createElement("li");
+var span = document.createElement("span");
+var checkbox = document.createElement("input");
+checkbox.type = "checkbox";
+
+
+
+
+
+
+var label = document.createElement("label");
+label.setAttribute("for","todo");
+
+var removeTodo = document.createElement("i");
+removeTodo.setAttribute("class", "fas fa-minus-square")
+
+addNote.addEventListener('click', addItem);
+input.addEventListener('keypress', addItemKeypress);
+
+function addItem() {
+    input.style.display = "block";
+    var item =input.value;
+    var textNode=document.createTextNode(item);
+
+    if(item==""){
+        return false;
+    }
+    else
+    {   
+        label.appendChild(textNode);
+        span.appendChild(checkbox);
+        span.appendChild(label);
+        li.appendChild(span);
+        li.appendChild(removeTodo);
+
+        ul.insertBefore(li,ul.childNodes[0]);
+        setTimeout(()=>{
+            li.className = "TodoTransi";
+        },2);
+        input.value="";
+        input.style.display = "none";
+
+    }
 }
-
-// input.addEventListener("keypress",addEle);
-
-// add.addEventListener("click",addEle);
-
-var cbElements = document.getElementsByClassName("checkbox1");
-
-for (var i = 0; i < cbElements.length; ++i) {
-  cbElements[i].addEventListener("click", function() {
-    this.getElementsByClassName("check")[0].classList.toggle('active');
-  });
-}
+//li:hover COLOR IS LEFT & LIST SCROLLBAR COLOR
